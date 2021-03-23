bfef7ac55d28d3cf05f39fc14aae0c9c811d330d
Kristian-Masso <kristian.masso@elev.ga.ntig.se>
2021-03-23 14:44:18 +0100

    fixade saker

---
+++ javascript-exempel.js
@@ -0,0 +1,8 @@
+﻿function authorDialog(){
+    alert("Kristian gjorde denna sida år 2021.");
+    changeText();
+}
+
+function changeText(){
+    document.getElementById("demo").innerHTML = "Hello Javascript.";
+}
